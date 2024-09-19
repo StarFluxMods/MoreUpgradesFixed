e5ae4b922217efdb334db5ab041ef47a666a53d5
Lachlan Leone <starfluxgames@gmail.com>
2024-09-20 07:56:58 +1000

    Initial Commit

---
+++ Menus/PreferenceMenu.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using Kitchen;
+using Kitchen.Modules;
+using KitchenLib;
+using KitchenLib.Preferences;
+using UnityEngine;
+
+namespace MoreUpgradesFixed.Menus
+{
+    public class PreferenceMenu : KLMenu<MenuAction>
+    {
+        public PreferenceMenu(bool isMainMenu, Transform container, ModuleList module_list) : base(container, module_list)
+        {
+            this.isMainMenu = isMainMenu;
+        }
+
+        private bool isMainMenu;
+
+        private Option<bool> WORKSTATION_TO_FREEZER = new Option<bool>(new List<bool> { true, false }, Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.WORKSTATION_TO_FREEZER).Value, new List<string> { "Enabled", "Disabled" });
+        private Option<bool> COMBINER_TO_PORTIONER = new Option<bool>(new List<bool> { true, false }, Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.COMBINER_TO_PORTIONER).Value, new List<string> { "Enabled", "Disabled" });
+        private Option<bool> TABLE_CYCLE = new Option<bool>(new List<bool> { true, false }, Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.TABLE_CYCLE).Value, new List<string> { "Enabled", "Disabled" });
+        private Option<bool> SHOE_CYCLE = new Option<bool>(new List<bool> { true, false }, Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.SHOE_CYCLE).Value, new List<string> { "Enabled", "Disabled" });
+        private Option<bool> UTILITY_CYCLE = new Option<bool>(new List<bool> { true, false }, Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.UTILITY_CYCLE).Value, new List<string> { "Enabled", "Disabled" });
+        private Option<bool> MOP_CYCLE = new Option<bool>(new List<bool> { true, false }, Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.MOP_CYCLE).Value, new List<string> { "Enabled", "Disabled" });
+
+        public override void Setup(int player_id)
+        {
+            base.Setup(player_id);
+
+            if (!isMainMenu)
+            {
+                AddInfo("Unfortunately these preferences can only be changed from the Main Menu.");
+            }
+            else
+            {
+                AddLabel("Workstation <-> Freezer");
+                AddSelect(WORKSTATION_TO_FREEZER);
+                WORKSTATION_TO_FREEZER.OnChanged += delegate(object _, bool result)
+                {
+                    Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.WORKSTATION_TO_FREEZER).Set(result);
+                    Mod.preferenceManager.Save();
+                };
+
+                New<SpacerElement>(true);
+
+                AddLabel("Combiner <-> Portioner");
+                AddSelect(COMBINER_TO_PORTIONER);
+                COMBINER_TO_PORTIONER.OnChanged += delegate(object _, bool result)
+                {
+                    Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.COMBINER_TO_PORTIONER).Set(result);
+                    Mod.preferenceManager.Save();
+                };
+
+                New<SpacerElement>(true);
+
+                AddLabel("Table Cycle");
+                AddSelect(TABLE_CYCLE);
+                TABLE_CYCLE.OnChanged += delegate(object _, bool result)
+                {
+                    Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.TABLE_CYCLE).Set(result);
+                    Mod.preferenceManager.Save();
+                };
+
+                New<SpacerElement>(true);
+
+                AddLabel("Shoe Cycle");
+                AddSelect(SHOE_CYCLE);
+                SHOE_CYCLE.OnChanged += delegate(object _, bool result)
+                {
+                    Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.SHOE_CYCLE).Set(result);
+                    Mod.preferenceManager.Save();
+                };
+
+                New<SpacerElement>(true);
+
+                AddLabel("Utility Cycle");
+                AddSelect(UTILITY_CYCLE);
+                UTILITY_CYCLE.OnChanged += delegate(object _, bool result)
+                {
+                    Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.UTILITY_CYCLE).Set(result);
+                    Mod.preferenceManager.Save();
+                };
+
+                New<SpacerElement>(true);
+
+                AddLabel("Mop Cycle");
+                AddSelect(MOP_CYCLE);
+                MOP_CYCLE.OnChanged += delegate(object _, bool result)
+                {
+                    Mod.preferenceManager.GetPreference<PreferenceBool>(Mod.MOP_CYCLE).Set(result);
+                    Mod.preferenceManager.Save();
+                };
+            }
+
+            New<SpacerElement>(true);
+            New<SpacerElement>(true);
+
+            AddButton(base.Localisation["MENU_BACK_SETTINGS"], delegate(int i)
+            {
+                Mod.preferenceManager.Save();
+                RequestPreviousMenu();
+            }, 0, 1f, 0.2f);
+        }
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,132 @@
+using System.Collections.Generic;
+using KitchenLib;
+using KitchenMods;
+using System.Reflection;
+using Kitchen;
+using KitchenData;
+using KitchenLib.Event;
+using KitchenLib.Preferences;
+using KitchenLib.References;
+using MoreUpgradesFixed.Menus;
+using KitchenLogger = KitchenLib.Logging.KitchenLogger;
+
+namespace MoreUpgradesFixed
+{
+    public class Mod : BaseMod, IModSystem
+    {
+        public const string MOD_GUID = "com.starfluxgames.moreupgradesfixed";
+        public const string MOD_NAME = "More Upgrades Fixed";
+        public const string MOD_VERSION = "0.1.0";
+        public const string MOD_AUTHOR = "StarFluxGames";
+        public const string MOD_GAMEVERSION = ">=1.2.0";
+
+        internal static KitchenLogger Logger;
+        internal static PreferenceManager preferenceManager;
+
+        internal static string WORKSTATION_TO_FREEZER = "WORKSTATION_TO_FREEZER";
+        internal static string COMBINER_TO_PORTIONER = "COMBINER_TO_PORTIONER";
+        internal static string TABLE_CYCLE = "TABLE_CYCLE";
+        internal static string SHOE_CYCLE = "SHOE_CYCLE";
+        internal static string UTILITY_CYCLE = "UTILITY_CYCLE";
+        internal static string MOP_CYCLE = "MOP_CYCLE";
+
+        public Mod() : base(MOD_GUID, MOD_NAME, MOD_AUTHOR, MOD_VERSION, MOD_GAMEVERSION, Assembly.GetExecutingAssembly())
+        {
+        }
+
+        protected override void OnInitialise()
+        {
+            Logger.LogWarning($"{MOD_GUID} v{MOD_VERSION} in use!");
+
+            if (preferenceManager.GetPreference<PreferenceBool>(WORKSTATION_TO_FREEZER).Value)
+            {
+                Appliance Workstation = GameData.Main.Get<Appliance>(ApplianceReferences.Workstation);
+                Appliance Freezer = GameData.Main.Get<Appliance>(ApplianceReferences.Freezer);
+
+                Workstation.Upgrades = new List<Appliance> { Freezer };
+                Freezer.Upgrades = new List<Appliance> { Workstation };
+            }
+
+            if (preferenceManager.GetPreference<PreferenceBool>(COMBINER_TO_PORTIONER).Value)
+            {
+                Appliance Combiner = GameData.Main.Get<Appliance>(ApplianceReferences.Combiner);
+                Appliance Portioner = GameData.Main.Get<Appliance>(ApplianceReferences.Portioner);
+
+                Combiner.Upgrades = new List<Appliance> { Portioner };
+                Portioner.Upgrades = new List<Appliance> { Combiner };
+            }
+
+            if (preferenceManager.GetPreference<PreferenceBool>(TABLE_CYCLE).Value)
+            {
+                Appliance TableBar = GameData.Main.Get<Appliance>(ApplianceReferences.TableBar);
+                Appliance TableCheapMetal = GameData.Main.Get<Appliance>(ApplianceReferences.TableCheapMetal);
+                Appliance TableBasicCloth = GameData.Main.Get<Appliance>(ApplianceReferences.TableBasicCloth);
+                Appliance TableFancyCloth = GameData.Main.Get<Appliance>(ApplianceReferences.TableFancyCloth);
+
+                TableBar.Upgrades = new List<Appliance> { TableCheapMetal };
+                TableCheapMetal.Upgrades = new List<Appliance> { TableBasicCloth };
+                TableBasicCloth.Upgrades = new List<Appliance> { TableFancyCloth };
+                TableFancyCloth.Upgrades = new List<Appliance> { TableBar };
+            }
+
+            if (preferenceManager.GetPreference<PreferenceBool>(SHOE_CYCLE).Value)
+            {
+                Appliance ShoeRackWellies = GameData.Main.Get<Appliance>(ApplianceReferences.ShoeRackWellies);
+                Appliance ShoeRackTrainers = GameData.Main.Get<Appliance>(ApplianceReferences.ShoeRackTrainers);
+                Appliance ShoeRackWorkBoots = GameData.Main.Get<Appliance>(ApplianceReferences.ShoeRackWorkBoots);
+
+                ShoeRackWellies.Upgrades = new List<Appliance> { ShoeRackTrainers };
+                ShoeRackTrainers.Upgrades = new List<Appliance> { ShoeRackWorkBoots };
+                ShoeRackWorkBoots.Upgrades = new List<Appliance> { ShoeRackWellies };
+            }
+
+            if (preferenceManager.GetPreference<PreferenceBool>(UTILITY_CYCLE).Value)
+            {
+                Appliance RollingPinProvider = GameData.Main.Get<Appliance>(ApplianceReferences.RollingPinProvider);
+                Appliance SharpKnifeProvider = GameData.Main.Get<Appliance>(ApplianceReferences.SharpKnifeProvider);
+                Appliance ScrubbingBrushProvider = GameData.Main.Get<Appliance>(ApplianceReferences.ScrubbingBrushProvider);
+
+                RollingPinProvider.Upgrades = new List<Appliance> { SharpKnifeProvider };
+                SharpKnifeProvider.Upgrades = new List<Appliance> { ScrubbingBrushProvider };
+                ScrubbingBrushProvider.Upgrades = new List<Appliance> { RollingPinProvider };
+            }
+
+            if (preferenceManager.GetPreference<PreferenceBool>(MOP_CYCLE).Value)
+            {
+                Appliance MopBucketLasting = GameData.Main.Get<Appliance>(ApplianceReferences.MopBucketLasting);
+                Appliance MopBucketFast = GameData.Main.Get<Appliance>(ApplianceReferences.MopBucketFast);
+                Appliance RobotMop = GameData.Main.Get<Appliance>(ApplianceReferences.RobotMop);
+                Appliance RobotBuffer = GameData.Main.Get<Appliance>(ApplianceReferences.RobotBuffer);
+
+                MopBucketLasting.Upgrades = new List<Appliance> { MopBucketFast };
+                MopBucketFast.Upgrades = new List<Appliance> { RobotMop };
+                RobotMop.Upgrades = new List<Appliance> { RobotBuffer };
+                RobotBuffer.Upgrades = new List<Appliance> { MopBucketLasting };
+            }
+        }
+
+        protected override void OnUpdate()
+        {
+        }
+
+        protected override void OnPostActivate(KitchenMods.Mod mod)
+        {
+            Logger = InitLogger();
+            preferenceManager = new PreferenceManager(MOD_GUID);
+            preferenceManager.RegisterPreference(new PreferenceBool(WORKSTATION_TO_FREEZER, false));
+            preferenceManager.RegisterPreference(new PreferenceBool(COMBINER_TO_PORTIONER, false));
+            preferenceManager.RegisterPreference(new PreferenceBool(TABLE_CYCLE, false));
+            preferenceManager.RegisterPreference(new PreferenceBool(SHOE_CYCLE, false));
+            preferenceManager.RegisterPreference(new PreferenceBool(UTILITY_CYCLE, false));
+            preferenceManager.RegisterPreference(new PreferenceBool(MOP_CYCLE, false));
+            preferenceManager.Load();
+            preferenceManager.Save();
+
+            Events.MainMenuView_SetupMenusEvent += (s, args) => { args.addMenu.Invoke(args.instance, new object[] { typeof(PreferenceMenu), new PreferenceMenu(true, args.instance.ButtonContainer, args.module_list) }); };
+
+            Events.PlayerPauseView_SetupMenusEvent += (s, args) => { args.addMenu.Invoke(args.instance, new object[] { typeof(PreferenceMenu), new PreferenceMenu(false, args.instance.ButtonContainer, args.module_list) }); };
+
+            ModsPreferencesMenu<MenuAction>.RegisterMenu(MOD_NAME, typeof(PreferenceMenu), typeof(MenuAction));
+        }
+    }
+}
